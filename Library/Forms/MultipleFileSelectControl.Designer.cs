dbc199aa477b5cc325127c45c32042de2f0639e9
lendres <lendres@fifthrace.com>
2016-04-19 12:40:17 -0500

    Multiple File Select Update

--- Library/Forms/MultipleFileSelectControl.Designer.cs
+++ Library/Forms/MultipleFileSelectControl.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace DigitalProduction
+﻿namespace DigitalProduction.Forms
 {
 	partial class MultipleFileSelectControl
 	{
@@ -28,19 +28,45 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.filesGroupBox = new System.Windows.Forms.GroupBox();
+			this.selectFilesButton = new System.Windows.Forms.Button();
 			this.removeFilesButton = new System.Windows.Forms.Button();
 			this.filesListBox = new System.Windows.Forms.ListBox();
-			this.selectFilesButton = new System.Windows.Forms.Button();
+			this.filesGroupBox.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// filesGroupBox
+			// 
+			this.filesGroupBox.Controls.Add(this.selectFilesButton);
+			this.filesGroupBox.Controls.Add(this.removeFilesButton);
+			this.filesGroupBox.Controls.Add(this.filesListBox);
+			this.filesGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.filesGroupBox.Location = new System.Drawing.Point(0, 0);
+			this.filesGroupBox.Name = "filesGroupBox";
+			this.filesGroupBox.Size = new System.Drawing.Size(510, 248);
+			this.filesGroupBox.TabIndex = 0;
+			this.filesGroupBox.TabStop = false;
+			this.filesGroupBox.Text = "Files";
+			// 
+			// selectFilesButton
+			// 
+			this.selectFilesButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+			this.selectFilesButton.Location = new System.Drawing.Point(6, 19);
+			this.selectFilesButton.Name = "selectFilesButton";
+			this.selectFilesButton.Size = new System.Drawing.Size(80, 23);
+			this.selectFilesButton.TabIndex = 7;
+			this.selectFilesButton.Text = "Select Files...";
+			this.selectFilesButton.UseVisualStyleBackColor = true;
 			// 
 			// removeFilesButton
 			// 
-			this.removeFilesButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.removeFilesButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.removeFilesButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.removeFilesButton.Location = new System.Drawing.Point(430, 0);
+			this.removeFilesButton.Enabled = false;
+			this.removeFilesButton.Location = new System.Drawing.Point(424, 19);
 			this.removeFilesButton.Name = "removeFilesButton";
 			this.removeFilesButton.Size = new System.Drawing.Size(80, 23);
-			this.removeFilesButton.TabIndex = 3;
+			this.removeFilesButton.TabIndex = 6;
 			this.removeFilesButton.Text = "Remove";
 			this.removeFilesButton.UseVisualStyleBackColor = true;
 			// 
@@ -50,41 +76,30 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.filesListBox.FormattingEnabled = true;
-			this.filesListBox.Location = new System.Drawing.Point(0, 36);
+			this.filesListBox.Location = new System.Drawing.Point(6, 56);
 			this.filesListBox.Name = "filesListBox";
 			this.filesListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
-			this.filesListBox.Size = new System.Drawing.Size(510, 212);
-			this.filesListBox.TabIndex = 2;
-			// 
-			// selectFilesButton
-			// 
-			this.selectFilesButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.selectFilesButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.selectFilesButton.Location = new System.Drawing.Point(0, 0);
-			this.selectFilesButton.Name = "selectFilesButton";
-			this.selectFilesButton.Size = new System.Drawing.Size(80, 23);
-			this.selectFilesButton.TabIndex = 4;
-			this.selectFilesButton.Text = "Select Files...";
-			this.selectFilesButton.UseVisualStyleBackColor = true;
-			this.selectFilesButton.Click += new System.EventHandler(this.selectFilesButton_Click);
+			this.filesListBox.Size = new System.Drawing.Size(498, 186);
+			this.filesListBox.TabIndex = 5;
 			// 
 			// MultipleFileSelectControl
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.selectFilesButton);
-			this.Controls.Add(this.removeFilesButton);
-			this.Controls.Add(this.filesListBox);
+			this.Controls.Add(this.filesGroupBox);
 			this.Name = "MultipleFileSelectControl";
 			this.Size = new System.Drawing.Size(510, 248);
+			this.filesGroupBox.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
+		private System.Windows.Forms.GroupBox filesGroupBox;
+		private System.Windows.Forms.Button selectFilesButton;
 		private System.Windows.Forms.Button removeFilesButton;
 		private System.Windows.Forms.ListBox filesListBox;
-		private System.Windows.Forms.Button selectFilesButton;
+
 	}
 }
---
+++ Library/Forms/MultipleFileSelectControl.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalProduction.Forms
+{
+	/// <summary>
+	/// A control for selecting multiple files and listing them in a ListBox.
+	/// </summary>
+	public partial class MultipleFileSelectControl : UserControl
+	{
+		#region Members
+
+		private string				_filterString;
+		private string				_browseForFilesTitle;
+		private string				_initialDirectory;
+
+		#endregion
+
+		#region Construction
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public MultipleFileSelectControl()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public MultipleFileSelectControl(string filterString)
+		{
+			InitializeComponent();
+			_filterString				= filterString;
+		}
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public MultipleFileSelectControl(string filterString, string browsingTitle)
+		{
+			InitializeComponent();
+			_filterString				= filterString;
+			_browseForFilesTitle		= browsingTitle;
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Filter string for file browsing.
+		/// </summary>
+		public string FilterString
+		{
+			get
+			{
+				return _filterString;
+			}
+
+			set
+			{
+				_filterString = value;
+			}
+		}
+
+		/// <summary>
+		/// Title for browsing for files.
+		/// </summary>
+		public string BrowseForFilesTitle
+		{
+			get
+			{
+				return _browseForFilesTitle;
+			}
+
+			set
+			{
+				_browseForFilesTitle = value;
+			}
+		}
+
+		/// <summary>
+		/// Initial directory for file browsing.
+		/// </summary>
+		public string InitialDirectory
+		{
+			get
+			{
+				return _initialDirectory;
+			}
+
+			set
+			{
+				_initialDirectory = value;
+			}
+		}
+
+		/// <summary>
+		/// Change the name of the GroupBox around the controls.
+		/// </summary>
+		public string GroupBoxText
+		{
+			get
+			{
+				return this.filesGroupBox.Text;
+			}
+
+			set
+			{
+				this.filesGroupBox.Text = value;
+			}
+		}
+
+		/// <summary>
+		/// Items selected in the form.
+		/// </summary>
+		public string[] SelectedFiles
+		{
+			get
+			{
+				ListBox.SelectedObjectCollection selectedItems = this.filesListBox.SelectedItems;
+
+				int size		= selectedItems.Count;
+				string[] files	= new string[size];
+
+				for (int i = 0; i < size; i++)
+				{
+					files[i] = selectedItems[i].ToString();
+				}
+
+				return files;
+			}
+		}
+
+		/// <summary>
+		/// Specifies if files have been selected.
+		/// </summary>
+		public bool FilesHaveBeenSelected
+		{
+			get
+			{
+				return this.filesListBox.Items.Count > 0;
+			}
+		}
+
+		#endregion
+
+		#region Event Handlers
+
+		/// <summary>
+		/// Browse for files event handler.
+		/// </summary>
+		/// <param name="sender">Sender.</param>
+		/// <param name="e">Event arguments.</param>
+		private void selectFilesButton_Click(object sender, EventArgs e)
+		{
+			string[] files = FileSelect.BrowseForMultipleFiles(this, _filterString, _browseForFilesTitle, _initialDirectory);
+
+			if (files != null)
+			{
+				this.filesListBox.Items.AddRange(files);
+			}
+		}
+
+		/// <summary>
+		/// Remove selected items from ListBox.
+		/// </summary>
+		/// <param name="sender">Sender.</param>
+		/// <param name="e">Event arguments.</param>
+		private void removeFilesButton_Click(object sender, EventArgs e)
+		{
+			ListBox.SelectedIndexCollection selectedIndices = this.filesListBox.SelectedIndices;
+			for (int i = this.filesListBox.SelectedIndices.Count-1; i >= 0; i--)
+			{
+				this.filesListBox.Items.RemoveAt(selectedIndices[i]);
+			}
+		}
+
+		#endregion
+
+		#region Methods
+
+		private void SetControls()
+		{
+			bool enabled					= this.filesListBox.Items.Count > 0;
+			this.removeFilesButton.Enabled	= enabled;
+		}
+
+		#endregion
+
+	} // End class.
+} // End namespace.
